66414aeb855a52c8867a65b1a4381b457cd3beca
Egor Levintsa <proldapru@yandex.ru>
2021-11-17 15:23:59 +0300

    Solved task wrappers/UiInputDate

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,5 +1,9 @@
 <template>
-  <ui-input />
+  <ui-input ref="uiInput" :type="type" v-model="modelValueProxy">
+    <template v-for="slotName of Object.keys($slots)" #[slotName]>
+      <slot :name="slotName" />
+    </template>
+  </ui-input>
 </template>
 
 <script>
@@ -9,5 +13,36 @@ export default {
   name: 'UiInputDate',
 
   components: { UiInput },
+
+  props: {
+    type: {
+      type: String,
+      default: 'date',
+      validator: value => ['date', 'time', 'datetime-local'].includes(value),
+    },
+    modelValue: {
+      type: Number,
+      default: null,
+    },
+  },
+
+  emits: ['update:modelValue'],
+
+  computed: {
+    modelValueProxy: {
+      get() {
+        if(!this.modelValue) return '';
+
+        const dateParts = new Date(this.modelValue).toISOString().split(/T|\./)
+        if(!this.$attrs.step || !(this.$attrs.step % 60)) dateParts[1] = dateParts[1].substr(0, 5)
+        return this.type==='date' ?
+          dateParts[0] :
+          (this.type==='time' ? dateParts[1] : `${dateParts[0]}T${dateParts[1]}`)
+      },
+      set(newValue) {
+        this.$emit('update:modelValue', this.$refs.uiInput.$refs.input.valueAsNumber)
+      },
+    },
+  },
 };
 </script>
