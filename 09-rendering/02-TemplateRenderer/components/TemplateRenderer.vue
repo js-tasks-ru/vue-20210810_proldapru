c9be836b5b33777fec73925e967b206705092a9a
Egor Levintsa <proldapru@yandex.ru>
2022-05-06 09:24:31 +0300

    Solved task TemplateRenderer

--- 09-rendering/02-TemplateRenderer/components/TemplateRenderer.vue
+++ 09-rendering/02-TemplateRenderer/components/TemplateRenderer.vue
@@ -1,5 +1,6 @@
 <script>
-// import { compile } from 'vue';
+import { compile, computed, resolveComponent, defineComponent, h } from 'vue';
+// import UiAlert from './UiAlert.vue';
 
 export default {
   name: 'TemplateRenderer',
@@ -19,6 +20,33 @@ export default {
       type: [Object, Array],
       default: () => [],
     },
+  },
+
+  // components: {UiAlert},
+
+  setup(props, context) {
+    /*** Так не сработало ***/
+    /*** Ошибка: Uncaught TypeError: bindings is undefined ***/
+    /*
+    const newComponent = defineComponent({
+      name: 'newComponent',
+      props: { bindings: {...props.bindings} },
+      components: {...props.components},
+      template: props.template,
+    });
+    return () => h(newComponent);
+    */
+
+    /*** Так сработало ***/
+    /**/
+    const renderFunction = computed(() => compile(props.template));
+    const newComponent = defineComponent({
+      name: 'newComponent',
+      components: {...props.components},
+      render: () => renderFunction.value.call(undefined, props),
+    });
+    return () => h(newComponent);
+    /**/
   },
 };
 </script>
