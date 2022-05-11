4650466817cd30785037a47a134c30600a8e9381
Egor Levintsa <proldapru@yandex.ru>
2022-05-11 11:37:55 +0300

    Solved task UiPaginationView

--- 10-slots/01-UiPaginationView/components/UiPaginationView.vue
+++ 10-slots/01-UiPaginationView/components/UiPaginationView.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="pagination-container">
     <!-- Контент страницы -->
+
+    <!-- Так не сработало -->
+    <!-- <slot v-for="meetup in pageMeetups" :item="meetup" /> -->
+
+    <!-- Так сработало -->
+    <template v-for="meetup in pageMeetups">
+      <slot :item="meetup" />
+    </template>
   </div>
 </template>
 
 <script>
+import { computed } from 'vue';
 export default {
   name: 'UiPaginationView',
 
@@ -23,6 +32,14 @@ export default {
       type: Array,
       required: true,
     },
+  },
+
+  setup(props) {
+    const pageMeetups = computed(() => {
+      // console.log(`${(props.page - 1) * props.perPage} ${props.page * props.perPage}`);
+      return props.items.filter((x, ind) => (ind >= (props.page - 1) * props.perPage) && (ind < props.page * props.perPage));
+    });
+    return { pageMeetups }
   },
 };
 </script>
